14393696bbe403db01c4fe4da440321614bd47ff
Samuel Kodytek <samuel.kodytek@guided.cz>
2023-04-29 14:43:05 +0200

    ...

---
+++ ahoj/test/Main.java
@@ -0,0 +1,7 @@
+package ahoj.test;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("How are you?");
+    }
+}
